75d2ea88ccd5d0ea9e2b6b5b6df6acee6764cc8d
Job Muturi <muirimuturi17@gmail.com>
2023-07-04 11:30:58 +0300

    Added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :chef_recipes
   resources :reviews
+  resources :chefs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
