94d58a46b042c837207fd2099e084711002ca3b3
laura-paullette <laura.paullette@student.moringaschool.com>
2023-07-04 13:13:09 +0300

    resource update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :chef_recipes
   resources :reviews
   resources :chefs
+  resources :recipe_tags
+  resources :tags
+  resources :recipes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
